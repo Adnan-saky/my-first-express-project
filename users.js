09b1ff5da3150bc03bbaa7818427bf6ac17dec5d
adnansaky <adnansaky123@gmail.com>
2023-01-19 22:08:24 +0600

    Update users.js

--- users.js
+++ users.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const users = require('/');
 
-const users = [
+/*const users = [
     {id:1, name:'adnan', email:'adnan@gmail.com'},
     {id:2, name:'saky', email:'saky@gmail.com'},
     {id:3, name:'nzf', email:'nzf@gmail.com'},
     {id:4, name:'srt', email:'srt@gmail.com'},
-]
+]*/
 
 const saveUsers = () => {
     fs.writeFileSync('users.json', JSON.stringify(users));
